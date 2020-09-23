9fd52cf1a05e7dab4b7dcbf53d78ee2e0fcf6c7a
Michael Vangelovski <michael.vangelovski@hotmail.com>
2020-09-23 11:02:24 +1000

    Fix overlapping on mobile.

--- db/mobile.php
+++ db/mobile.php
@@ -31,8 +31,8 @@ $addons = array(
                     'class' => 'block_my_day_timetable'
                 ),
                 'styles' => [
-                    'url' => $CFG->wwwroot . '/blocks/my_day_timetable/mobileapp.css?v=2020091600',
-                    'version' => 2020091600
+                    'url' => $CFG->wwwroot . '/blocks/my_day_timetable/mobileapp.css?v=2020092300',
+                    'version' => 2020092300
                 ],
                 'delegate' => 'CoreBlockDelegate', // Delegate (where to display the link to the add-on)
                 'method' => 'timetable_view', // Main function in \block_my_day_timetable\output\mobile
